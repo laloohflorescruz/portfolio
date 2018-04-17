eee619735fc1a8c9889b465f5e198d4cc4272b3c
eflores <eflores@digepres.gob.do>
2018-04-17 14:17:13 -0400

    Animations

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,3 +1,4 @@
+import { InfomationService } from './../../services/infomation.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   anio:number = new Date().getFullYear();
-  constructor() { }
+
+  constructor(public _is: InfomationService) { }
 
   ngOnInit() {
   }
--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,3 +1,4 @@
+import { InfomationService } from './../../services/infomation.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _is: InfomationService) { }
 
   ngOnInit() {
   }
