4e2716f80e6f72454b469773ea91d5c5d67a76d0
eflores <eflores@digepres.gob.do>
2018-04-18 09:00:53 -0400

    Firebase service online

--- src/app/services/infomation.service.ts
+++ src/app/services/infomation.service.ts
@@ -6,9 +6,15 @@ export class InfomationService {
 
   info: any = {};
   loaded: false;
+  loaded_about: true;
+  team: any = {};
 
   constructor( public http: Http ) {
+    this.data_info();
+    this.data_about_us();
+  }
 
+   public data_info() {
     this.http.get('assets/data/info.pagina.json')
     .subscribe( data => {
       console.log(data.json());
@@ -16,4 +22,15 @@ export class InfomationService {
       this.info = data.json();
     });
    }
+
+
+   public data_about_us() {
+    this.http.get('https://portfolio-5b914.firebaseio.com/Team.json')
+    .subscribe( data => {
+      //console.log(data.json());
+      this.loaded_about = true;
+      this.team = data.json();
+    });
+   }
+
 }
